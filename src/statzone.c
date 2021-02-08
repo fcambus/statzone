33112a326b32a387b51c6660dd5260b41773b354
Frederic Cambus <fred@statdns.com>
2021-02-08 14:27:38 +0100

    Remove useless braces.

--- src/statzone.c
+++ src/statzone.c
@@ -190,13 +190,11 @@ main(int argc, char *argv[])
 				continue;
 			}
 
-			if (token_count && !strcmp(token_lc, "a")) {
+			if (token_count && !strcmp(token_lc, "a"))
 				results.a++;
-			}
 
-			if (token_count && !strcmp(token_lc, "aaaa")) {
+			if (token_count && !strcmp(token_lc, "aaaa"))
 				results.aaaa++;
-			}
 
 			if (token_count && !strcmp(token_lc, "ds")) {
 				results.ds++;
