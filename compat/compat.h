9127ec4da8e104749032144ccd848251ccab46b9
Frederic Cambus <fred@statdns.com>
2021-03-30 19:36:06 +0200

    Remove now unneeded CLOCK_MONOTONIC fallback macro in compat header.

--- compat/compat.h
+++ compat/compat.h
@@ -5,9 +5,4 @@
 #include "pledge.h"
 #endif
 
-/* Use CLOCK_REALTIME if CLOCK_MONOTONIC is not available */
-#ifndef CLOCK_MONOTONIC
-#define CLOCK_MONOTONIC CLOCK_REALTIME
-#endif
-
 #endif /* COMPAT_H */
